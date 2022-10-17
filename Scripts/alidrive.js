8857b529522fad5e77ccec3699aeb2ace0f28938
Softlyx <109867095+Softlyx@users.noreply.github.com>
2022-10-17 16:17:08 +0800

    Update alidrive.js

--- Scripts/alidrive.js
+++ Scripts/alidrive.js
@@ -4,13 +4,12 @@
 > 更新时间：2022-10-16
 > 脚本功能：优化首页display
 
-***********************************/
 
 
 [rewrite_local]
 
-# ～ 阿里云盘（2022-10-16）@ddgksf2013
-^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://github.com/ddgksf2013/Cuttlefish/raw/master/Script/alidrive.js
+# ～ 阿里云盘（2022-10-16）
+^https?:\/\/api\.aliyundrive\.com\/apps\/v\d\/users\/apps\/widgets$ url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Scripts/alidrive.js
 
 [mitm]
 
@@ -18,8 +17,8 @@ hostname=api.aliyundrive.com
 
 ***********************************/
 
-let ddgksf2013 = JSON.parse($response.body);
-if (ddgksf2013.result) {
-    ddgksf2013.result = Object.values(ddgksf2013.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
+let Softly = JSON.parse($response.body);
+if (Softly.result) {
+    Softly.result = Object.values(Softly.result).filter(item => (item["appCode"]=="file" || item["appCode"]=="video"));
 }
-$done({ body: JSON.stringify(ddgksf2013) });
+$done({ body: JSON.stringify(Softly) });
