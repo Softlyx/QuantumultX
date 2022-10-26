8f30504e40cf2c1f0af6e81111cf9b9efc2d6b95
Softlyx <109867095+Softlyx@users.noreply.github.com>
2022-10-26 14:22:23 +0800

    Update AoRuanKouTu.js

--- Crack/AoRuanKouTu.js
+++ Crack/AoRuanKouTu.js
@@ -3,17 +3,13 @@
 > 应用名称：傲软抠图
 > 软件版本：1.6.0
 > 下载地址：https://apps.apple.com/cn/app/id1490054676
-> 脚本作者：Cuttlefish
-> 微信账号：墨鱼手记
 > 更新时间：2022-09-13
-> 通知频道：https://t.me/ddgksf2021
-> 问题反馈：https://t.me/ddgksf2013_bot
 > 特别说明：本脚本仅供学习交流使用，禁止转载售卖
  
 [rewrite_local]
 
 # ～ 傲软抠图解锁会员权限（2022-09-13）@ddgksf2013
-https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/apowersoft.js
+https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AoRuanKouTu.js
 
 [mitm] 
 
@@ -23,7 +19,7 @@ hostname=*.aoscdn.com
 
 
 
-var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣","tgchannel":"https://t.me/ddgksf2021","feedback":"https://t.me/ddgksf2013_bot"};
+var cuttlefish ={"warning":"本腳本僅供學習交流使用，禁止轉載售賣"};
 var ddgksf2013 = {
   "status" : 200,
   "message" : "success",
@@ -46,4 +42,4 @@ var ddgksf2013 = {
     "vip_special" : 1
   }
 };
-$done({body: JSON.stringify(ddgksf2013)});
+$done({body: JSON.stringify(Softly)});
