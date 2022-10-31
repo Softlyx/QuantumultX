9bc815bc13b598cc0be86ac0c59fed1b1f5e9d46
Softlyx <109867095+Softlyx@users.noreply.github.com>
2022-10-31 15:29:12 +0800

    Update AoRuanKouTu.js

--- Crack/AoRuanKouTu.js
+++ Crack/AoRuanKouTu.js
@@ -1,25 +1,28 @@
-/***********************************
+/****************************************
 
-> 应用名称：傲软
-> 更新时间：2022-10-31
-> 特别说明：本脚本仅供学习交流使用，禁止转载售卖
- 
+项目功能：傲软全家福 解锁VIP
+下载地址：http://mtw.so/5ukhax
+使用声明：仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*****************************************
+
 [rewrite_local]
 
-# ～ 傲软解锁会员权限
-^https?:\/\/gw\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AoRuanKouTu.js
-^https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations$ url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AoRuanKouTu.js
+^https?:\/\/.*\.aoscdn\.com\/base\/vip\/client\/authorizations url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AoRuanKouTu.js
 
-[mitm] 
+[mitm]
 
-hostname=gw.aoscdn.com
+hostname = *.aoscdn.com
 
-***********************************/
-
-
+****************************************/
 
 var body = $response.body; 
- var objk = JSON.parse(body); 
-   objk = {"status":200,"message":"success","data":{"license_type":"free","period_type":"trial","is_activated":1,"remain_days":0,"will_expire":1,"allowed_device_count":1,"begin_activated_time":1662783235,"durations":0,"vip_special":0,"has_buy_extend":0,"has_present":0,"product_id":369,"is_lifetime":0,"expired_at":253395492741,"expire_time":"9999-01-01 00:00:00","candy":0,"candy_expired_at":0,"device_id":2700581012,"exist_trial":0}} 
- body = JSON.stringify(objk); 
-   $done({body}); 
+var obj = JSON.parse(body); 
+
+obj.data.is_activated = 1;
+obj.data.expire_time = "2099-10-23 09:09:09";
+obj.data.durations = 99999;
+obj.data.expired_at = 253395492741;
+
+body = JSON.stringify(obj);
+$done({body});
