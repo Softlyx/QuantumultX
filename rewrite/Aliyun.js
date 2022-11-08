f1e7eaa10f2dbcedfb30487257ed5f670356b2da
Softlyx <109867095+Softlyx@users.noreply.github.com>
2022-11-08 19:52:26 +0800

    Update Aliyun.js

--- rewrite/Aliyun.js
+++ rewrite/Aliyun.js
@@ -12,7 +12,7 @@
 
 [rewrite_local]
 
-^https?:\/\/.*\.example\.com url script-analyze-echo-response https://raw.githubusercontent.com/Softlyx/QuantumultX/main/rewrite/aliyun.js
+^https?:\/\/.*\.example\.com url script-analyze-echo-response https://raw.githubusercontent.com/Softlyx/QuantumultX/main/rewrite/Aliyun.js
 
 [mitm]
 
