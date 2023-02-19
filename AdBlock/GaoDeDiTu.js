dd0c120aa26f16f104af94c69bf511cabe947ee9
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-02-19 09:48:29 +0800

    Update GaoDeDiTu.js

--- AdBlock/GaoDeDiTu.js
+++ AdBlock/GaoDeDiTu.js
@@ -15,29 +15,29 @@ hostname=*.amap.com
 
 
 # > 高德地图_底部tab栏角标
-^https?:\/\/.*\.amap\.com\/ws\/message\/notice\/list url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/message\/notice\/list url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_路线规划下方广告
 ^https?:\/\/.*\.amap\.com\/ws\/faas\/amap-navigation\/card-service-route-plan\? url reject-dict
 # > 高德地图_首页右中广告
-^https?:\/\/.*\.amap\.com\/ws\/shield\/frogserver\/aocs\/updatable url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/shield\/frogserver\/aocs\/updatable url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_附近页面
-^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/nearbyrec_smart url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/nearbyrec_smart url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_打车页面处理
-^https?:\/\/.*\.amap\.com\/ws\/promotion-web\/resource url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/promotion-web\/resource url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_Tip请求
 ^https?:\/\/.*\.amap\.com\/ws\/boss\/order_web\/\w{8}_information url reject-200
 # > 高德地图_首页底部处理
-^https?:\/\/.*\.amap\.com\/ws\/faas\/amap-navigation\/main-page url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/faas\/amap-navigation\/main-page url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_通用广告请求
 ^https?:\/\/.*\.amap\.com\/ws\/asa\/ads_attribution url reject
 # > 高德地图_开屏广告
-^https?:\/\/.*\.amap\.com\/ws\/valueadded\/alimama\/splash_screen url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/valueadded\/alimama\/splash_screen url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_首页顶部推广
-^https?:\/\/.*\.amap\.com\/ws\/msgbox\/pull url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/msgbox\/pull url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_我的页面
-^https?:\/\/.*\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaas url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaas url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_搜索框热词
-^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/new_hotword url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/amap.js
+^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/new_hotword url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
 # > 高德地图_搜索框scene
 ^https?:\/\/.*\.amap\.com\/ws\/shield\/scene\/recommend url reject-dict
 # > 高德地图_首页左上角gif
