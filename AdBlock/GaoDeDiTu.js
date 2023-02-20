4977e5810e50c4a02d7a06891b02abd069d0dad9
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-02-20 08:23:09 +0800

    Update GaoDeDiTu.js

--- AdBlock/GaoDeDiTu.js
+++ AdBlock/GaoDeDiTu.js
@@ -1,5 +1,6 @@
 # ======= 高德地图 ======= #
 # 2023-02-19 15:20
+# 原链接 https://github.com/RuCu6/QuanX/tree/main/Rewrites/Cube
 
 # 导航详情页,路线规划,地点详情页
 ^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/card-service-route-plan\? url reject-dict
