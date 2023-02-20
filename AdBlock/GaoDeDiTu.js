1222795d051992904550870033894d0c4fb37094
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-02-20 08:19:22 +0800

    Update GaoDeDiTu.js

--- AdBlock/GaoDeDiTu.js
+++ AdBlock/GaoDeDiTu.js
@@ -1,48 +1,39 @@
-// ==UserScript==
-// @ScriptName        高德地图☆净化
-// @UpdateTime        2023-02-08
-// @Suitable          自行观看“# > ”注释内容
-// @Attention         使用前请卸载高德地图，挂载脚本然后使用
-// @Version           V1.0.19
-// @ScriptURL         https://github.com/ddgksf2013/Rewrite/raw/master/AdBlock/Amap.conf
-// ==/UserScript==
+# ======= 高德地图 ======= #
+# 2023-02-19 15:20
 
+# 导航详情页,路线规划,地点详情页
+^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/card-service-route-plan\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/shield\/search\/poi\/detail\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/m5\.amap\.com\/ws\/shield\/search_poi\/(city_feed|tips_adv)\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/shield\/search_poi\/tips_operation_location\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
 
-hostname=*.amap.com
+# 首页,卡片,红点角标,右上角动图,搜索框热词,左下角天气,天气动效图层,消息横幅
+^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/main-page\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/main-page-(assets|location)\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/(mapapi\/hint_text\/offline_data|message\/notice\/list|shield\/search\/new_hotword)\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/mapapi\/poi\/infolite\/\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/m5\.amap\.com\/ws\/shield\/frogserver\/aocs\/updatable\/1\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/m5\.amap\.com\/ws\/valueadded\/weather\/v2\? url reject-dict
+^https:\/\/mps\.amap\.com\/ws\/mps\/scene\? url reject
+^https:\/\/sns\.amap\.com\/ws\/msgbox\/pull(_mp)?\? url reject-dict
 
-# > 高德地图_请自行添加本地分流
-;host, amdc.m.taobao.com, reject
+# 我的页面,个人主页,卡片,足迹
+^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/usr-profile-fc\/home\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaasv3\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/sns\.amap\.com\/ws\/userview\/footprint url reject-dict
 
+# 附近页
+^https:\/\/m5\.amap\.com\/ws\/shield\/search\/nearbyrec_smart\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
 
-# > 高德地图_底部tab栏角标
-^https?:\/\/.*\.amap\.com\/ws\/message\/notice\/list url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_路线规划下方广告
-^https?:\/\/.*\.amap\.com\/ws\/faas\/amap-navigation\/card-service-route-plan\? url reject-dict
-# > 高德地图_首页右中广告
-^https?:\/\/.*\.amap\.com\/ws\/shield\/frogserver\/aocs\/updatable url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_附近页面
-^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/nearbyrec_smart url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_打车页面处理
-^https?:\/\/.*\.amap\.com\/ws\/promotion-web\/resource url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_Tip请求
-^https?:\/\/.*\.amap\.com\/ws\/boss\/order_web\/\w{8}_information url reject-200
-# > 高德地图_首页底部处理
-^https?:\/\/.*\.amap\.com\/ws\/faas\/amap-navigation\/main-page url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_通用广告请求
-^https?:\/\/.*\.amap\.com\/ws\/asa\/ads_attribution url reject
-# > 高德地图_开屏广告
-^https?:\/\/.*\.amap\.com\/ws\/valueadded\/alimama\/splash_screen url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_首页顶部推广
-^https?:\/\/.*\.amap\.com\/ws\/msgbox\/pull url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_我的页面
-^https?:\/\/.*\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaas url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_搜索框热词
-^https?:\/\/.*\.amap\.com\/ws\/shield\/search\/new_hotword url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/AdBlock/GaoDeDiTu.js
-# > 高德地图_搜索框scene
-^https?:\/\/.*\.amap\.com\/ws\/shield\/scene\/recommend url reject-dict
-# > 高德地图_首页左上角gif
-^https?:\/\/.*\.amap\.com\/uploadimg\/\w+\.gif url reject-img
-# > 高德地图_首页右中'2022出行报告'[已从根本上解决]
-;^https?:\/\/img\.alicdn\.com\/imgextra\/i1\/O1CN01z5u64r1EdKvtRa19q_!!6000000000374-2-tps-120-120\.png
-# > 高德地图_左下天气
-^https?:\/\/.*\.amap\.com\/ws\/valueadded\/weather url reject-dict
+# 其他
+^https:\/\/m5\.amap\.com\/ws\/asa\/ads_attribution\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/shield\/scene\/recommend\? url reject-dict
+
+# 开屏广告
+^https:\/\/m5\.amap\.com\/ws\/valueadded\/alimama\/splash_screen\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+
+# 打车页,红点角标,弹窗,卡片
+^https:\/\/m5-zb\.amap\.com\/ws\/boss\/order(_web\/friendly_information|\/car\/king_toolbox_car_bubble)\? url reject-dict
+^https:\/\/m5-zb\.amap\.com\/ws\/promotion-web\/resource\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+
+hostname = m5.amap.com, m5-zb.amap.com, mps.amap.com, sns.amap.com
