541a98105a8cb52648f33a9b9f9fab1eff05fc7c
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-03-08 08:38:47 +0800

    Update GaoDeDiTu.js

--- AdBlock/GaoDeDiTu.js
+++ AdBlock/GaoDeDiTu.js
@@ -1,6 +1,6 @@
 # 原链接 https://github.com/RuCu6/QuanX/tree/main/Rewrites/Cube
 # ======= 高德地图 ======= #
-# 2023-02-21 19:00
+# 2023-03-07 18:25
 
 # 导航详情页,路线规划,地点详情页
 ^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/card-service-route-plan\? url reject-dict
@@ -20,7 +20,7 @@
 ^https:\/\/sns\.amap\.com\/ws\/msgbox\/pull(_mp)?\? url reject-dict
 
 # 我的页面,个人主页,卡片,足迹
-^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/usr-profile-fc\/home\? url reject-dict
+^https:\/\/m5\.amap\.com\/ws\/faas\/amap-navigation\/usr-profile-fc\/(highlightV2|home)\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
 ^https:\/\/m5\.amap\.com\/ws\/shield\/dsp\/profile\/index\/nodefaasv3\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
 ^https:\/\/sns\.amap\.com\/ws\/userview\/footprint url reject-dict
 
@@ -36,6 +36,6 @@
 
 # 打车页,红点角标,弹窗,卡片
 ^https:\/\/m5-zb\.amap\.com\/ws\/boss\/order(_web\/friendly_information|\/car\/king_toolbox_car_bubble)\? url reject-dict
-^https:\/\/m5-zb\.amap\.com\/ws\/promotion-web\/resource\? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
+^https:\/\/m5-zb\.amap\.com\/ws\/promotion-web\/resource\?? url script-response-body https://raw.githubusercontent.com/RuCu6/QuanX/main/Scripts/amap.js
 
 hostname = m5.amap.com, m5-zb.amap.com, mps.amap.com, sns.amap.com
