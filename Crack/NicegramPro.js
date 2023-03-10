065361d9628f3994227c1a3dbc11d60ba47df937
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-03-10 10:37:02 +0800

    Create NicegramPro.js

---
+++ Crack/NicegramPro.js
@@ -0,0 +1,15 @@
+/******************************
+
+脚本功能：Nicegram: Unlimited Text+解锁订阅
+
+*******************************
+
+[rewrite_local]
+
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/Nicegram.js
+
+[mitm] 
+hostname=restore-access.indream.app
+
+
+*******************************/
