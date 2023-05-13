637a459f4ea26e45955c68e57b7a7af22191581b
Softlyx <109867095+Softlyx@users.noreply.github.com>
2023-05-14 00:43:54 +0800

    Update Sinaemail.js

--- AdBlock/Sinaemail.js
+++ AdBlock/Sinaemail.js
@@ -1,8 +1,6 @@
 [rewrite_local]
-
-^https:\/\/.*\.gdt\.qq\.com\/gdt_mview.fcg url reject-200
-^https?:\/\/api-access\.pangolin-sdk-toutiao\.com\/api\/ad\/union\/sdk\/get_ads url reject
-
+^https:\/\/.*\.gdt\.qq\.com\/gdt_mview.fcg url reject
+^https?:\/\/.*\.pangolin-.*toutiao.*\.com url reject
 [mitm]
 
-hostname = api-access.pangolin-sdk-toutiao.com,*.gdt.qq.com
+hostname = *.pangolin-*toutiao*.com,*.gdt.qq.com
