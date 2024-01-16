108b28928d24a815310bf2ca460a44e57302f5b4
Softlyx <109867095+Softlyx@users.noreply.github.com>
2024-01-16 14:25:31 +0800

    Update AppRaven.js

--- Crack/AppRaven.js
+++ Crack/AppRaven.js
@@ -1,16 +1,23 @@
 /***********************************************
+
+> 应用名称：墨鱼自用AppRaven脚本
+> 脚本作者：@ddgksf2013
+> 微信账号：墨鱼手记
+> 更新时间：2023-07-24
+> 通知频道：https://t.me/ddgksf2021
+> 贡献投稿：https://t.me/ddgksf2013_bot
+> 问题反馈：ddgksf2013@163.com
+> 特别提醒：如需转载请注明出处，谢谢合作！
 > 解锁功能：界面美化[主界面APP应用内购买灰色框太难看了]、使用会员图标功能
 
 
 [rewrite_local]
 
-^https?:\/\/.*appraven\.net\/appraven\/graphql url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AppRaven.js
-^https?:\/\/v2\.appraven\.net\/appraven\/graphql url script-response-body https://raw.githubusercontent.com/Softlyx/QuantumultX/main/Crack/AppRaven.js
-
+^https?:\/\/v2\.appraven\.net\/appraven\/graphql url script-response-body https://gist.githubusercontent.com/ddgksf2013/562f8fd6a40be92d7c8b358aa25eab3f/raw/appraven.vip.js
 
 [mitm]
 
-hostname = *appraven.net,v2.appraven.net
+hostname = v2.appraven.net
 
 ***********************************************/
 
